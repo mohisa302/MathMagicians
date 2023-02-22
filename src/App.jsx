5c680f8ead5d39711a05e0986a14f8ebd5333521
mohi <mohisadeghi302@gmail.com>
2023-02-22 21:04:46 +0330

    Add browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Quotes from './components/Quotes';
 import Calculator from './components/Calculator';
@@ -7,12 +7,14 @@ import Header from './components/Header';
 
 const App = () => (
   <div className="pageContainer">
-    <Header />
-    <Routes>
-      <Route path="MathMagicians/" element={<Home />} />
-      <Route path="MathMagicians/calculate" element={<Calculator />} />
-      <Route path="MathMagicians/quotes" element={<Quotes />} />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="MathMagicians/" element={<Home />} />
+        <Route path="MathMagicians/calculate" element={<Calculator />} />
+        <Route path="MathMagicians/quotes" element={<Quotes />} />
+      </Routes>
+    </BrowserRouter>
   </div>
 );
 export default App;
