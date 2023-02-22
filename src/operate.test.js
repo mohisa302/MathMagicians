6c1dd61971dfe5a68548607c16bdbf36d7f8e90a
mohi <mohisadeghi302@gmail.com>
2023-02-22 16:40:41 +0330

    Fix linters error

--- src/operate.test.js
+++ src/operate.test.js
@@ -1,19 +1,18 @@
 import operate from './logic/operate';
 
 describe('Calculator', () => {
+  it('check operate add functionality', () => {
+    const result = operate(2, 3, '+');
+    expect(result).toBe('5');
+  });
 
-    it(`check operate add functionality`, () => {
-        const result = operate(2, 3, '+')
-        expect(result).toBe("5");
-    });
+  it('check operate subtrack functionality', () => {
+    const result = operate(2, 3, '-');
+    expect(result).toBe('-1');
+  });
 
-    it(`check operate add functionality`, () => {
-        const result = operate(2, 3, '-')
-        expect(result).toBe("-1");
-    });
-
-    it(`check operate devision functionality`, () => {
-        const result = operate(10, 2, '÷')
-        expect(result).toBe("5");
-    });
+  it('check operate devision functionality', () => {
+    const result = operate(10, 2, '÷');
+    expect(result).toBe('5');
+  });
 });
