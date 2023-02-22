7fe8eadc8c4c62f70a3bde5df0342bb6ddcf368b
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-02-22 15:18:15 +0200

    Add operate multiplication test

--- src/operate.test.js
+++ src/operate.test.js
@@ -1,19 +1,23 @@
 import operate from './logic/operate';
 
 describe('Calculator', () => {
+  it(`check operate add functionality`, () => {
+    const result = operate(2, 3, '+');
+    expect(result).toBe('5');
+  });
 
-    it(`check operate add functionality`, () => {
-        const result = operate(2, 3, '+')
-        expect(result).toBe("5");
-    });
+  it(`check operate add functionality`, () => {
+    const result = operate(2, 3, '-');
+    expect(result).toBe('-1');
+  });
 
-    it(`check operate add functionality`, () => {
-        const result = operate(2, 3, '-')
-        expect(result).toBe("-1");
-    });
+  it(`check operate division functionality`, () => {
+    const result = operate(10, 2, '÷');
+    expect(result).toBe('5');
+  });
 
-    it(`check operate devision functionality`, () => {
-        const result = operate(10, 2, '÷')
-        expect(result).toBe("5");
-    });
+  it(`check operate multiplication functionality`, () => {
+    const result = operate(2, 2, 'x');
+    expect(result).toBe('4');
+  });
 });
